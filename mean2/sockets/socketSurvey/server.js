d8e2e32ffa0dd19c490a4dbe53c20f2971d5881b
Ronald Guerrero <ronald.a.guerrero@gmail.com>
2019-05-12 17:24:23 -0700

    uploading files

---
+++ mean2/sockets/socketSurvey/server.js
@@ -0,0 +1,46 @@
+// Load the express module and store it in the variable express (Where do you think this comes from?)
+var express = require("express");
+// console.log("Let's find out what express is", express);
+// invoke express and store the result in the variable app
+var app = express();
+// console.log("Let's find out what app is", app);
+// use app's get method and pass it the base route '/' and a callback
+// require body-parser
+var bodyParser = require('body-parser');
+// use it!
+
+app.use(express.static(__dirname + "/public"));
+const server = app.listen(1337);
+const io = require('socket.io')(server);
+var counter = 0;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(request, response) {
+   response.render("index");
+})
+
+
+
+    
+io.on('connection', function (socket) { //2
+  socket.on('posting_form', function (data) { //7
+    console.log(data); //8 (note: this log will be on your server's terminal)
+    let randomNum = Math.floor((Math.random() * 1000) + 1);
+    socket.emit('updated_message', {res: data});
+    socket.emit('random_number', {res: randomNum});
+  });
+});
+
+
+
+
+// this is the line that tells our server to use the "/static" folder for static content
+app.use(express.static(__dirname + "/static"));
+// two underscores before dirname
+// try printing out __dirname using console.log to see what it is and why we use it
+
+// This sets the location where express will look for the ejs views
+app.set('views', __dirname + '/views'); 
+// Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
+app.set('view engine', 'ejs');
