435f22c58bc72e9f54d06eea7f9e4fb6d4d9b8ef
Ronald Guerrero <ronald.a.guerrero@gmail.com>
2019-06-04 20:12:43 -0700

    adding algo

--- python2/python/fundamentals/selection_sort.py
+++ python2/python/fundamentals/selection_sort.py
@@ -1,19 +1,20 @@
-arr = [5,3,6,2,1]
+arr = [64, 25, 12, 22, 11] 
+  
+# Traverse through all array elements 
+def select_sort(A):
+	for i in range(len(A)): 
+	      
+	    # Find the minimum element in remaining  
+	    # unsorted array 
+	    min_idx = i 
+	    for j in range(i+1, len(A)): 
+	        if A[min_idx] > A[j]: 
+	            min_idx = j 
+	              
+	    # Swap the found minimum element with  
+	    # the first element         
+	    A[i], A[min_idx] = A[min_idx], A[i]
 
-def selectionsort(arr):
-	# loop through
-	for i in range(len(arr)):
-		# loop again
-		for j in range(i+1, len(arr)):
-			# check if i is greater than j
-			if arr[i] > arr[j]:
-				# swap
-				temp = arr[i]
-				arr[i] = arr[j]
-				arr[j] = temp
-				print(arr)
+	print(A)
 
-	return arr
-
-selectionsort(arr);
-		
+select_sort(arr) 
