52105ea518a9aee96b3fc071df78cf4bdaec4867
Ronald Guerrero <ronald.a.guerrero@gmail.com>
2019-06-13 14:29:16 -0700

    adding algo

--- python2/python/fundamentals/test.py
+++ python2/python/fundamentals/test.py
@@ -1,11 +1,24 @@
-def bubble_sort(arr):
-	for i in range(len(arr)):		
-		for j in range(len(arr)-1-i):
-			if(arr[j] > arr[j+1]):
-				arr[j], arr[j+1] = arr[j+1], arr[j]
-	print(arr)
+# def bubble_sort(arr):
+# 	for i in range(len(arr)):		
+# 		for j in range(len(arr)-1-i):
+# 			if(arr[j] > arr[j+1]):
+# 				arr[j], arr[j+1] = arr[j+1], arr[j]
+# 	print(arr)
 	
-bubble_sort([5,3,2,1,4])
+# bubble_sort([5,3,2,1,4])
 
-def selection_sort(arr):
-	
+# def selection_sort(arr):
+
+def insertion_sort(arr):
+	for i in range(len(arr)):
+		j = i - 1
+		iVal = arr[i]
+		while(j > 0 and iVal > arr[j]):
+			arr[j+1] = arr[j]
+			j-=1
+		arr[j+1] = iVal
+	print(arr)
+
+insertion_sort([5,3,2,1,4])
+
+s
