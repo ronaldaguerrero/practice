e5cf34f793edecef766452c86a5ac33b6ed2f869
Ronald Guerrero <ronald.a.guerrero@gmail.com>
2019-06-19 14:16:26 -0700

    adding algo

---
+++ Algos/sigma.js
@@ -0,0 +1,15 @@
+function sigma(num){
+	var sum = 0;
+	for(var i = 1; i <= num; i++){
+		sum+= i;
+	}
+	console.log(sum);
+}
+
+function factorial(num){
+	var sum = 0;
+	for(var i = 1; i <= num; i++){
+		sum*= i;
+	}	
+	console.log(sum);
+}
